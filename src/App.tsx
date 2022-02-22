4975f2fbd35c4dd986795cfd24bf9f7b5270ace1
Vanessa Elizabeth Piccoli <54444671+vaneep@users.noreply.github.com>
2022-02-21 22:57:17 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import GlobalStyle from './styles/GlobalStyle';
+
+function App() {
+  return (
+<>
+<h1>Vanessa</h1>
+<GlobalStyle/>
+</>
+    
+  );
+}
+
+export default App;
---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+//SL - SERVER LIST
+//SN -SERVER NAME
+//CI - Channel info
+
+export const Grid = styled.div `
+display: grid;
+
+grid-template-column: ;
+grid-template-rows: ;
+grid-template-areas: 
+'SL SN CI CI' 
+'SL SN CI CI' 
+
+;
+
+`;
